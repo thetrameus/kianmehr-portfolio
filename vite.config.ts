8450fb3092d921008e38be24e9015c422c0581f5
unknown <hamed.sh0513@gmail.com>
2025-08-06 03:03:23 +0330

    Fix SPA 404 on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,12 @@ export default defineConfig({
   },
   plugins: [react(), tailwindcss()],
   base: "/kianmehr-portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
